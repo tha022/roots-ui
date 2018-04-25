01a9542c3c349d1afad90cf3871d0bf5dcea4214
Thomas Vervik <thomas.vervik@gmail.com>
2018-04-25 12:55:29 +0300

    Added express and right build / run instructions in packages.json

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-"scripts": {
-  "start": "node app.js"
-}
+web: npm start
---
+++ app.js
@@ -0,0 +1,17 @@
+//var proxy = require('express-http-proxy');
+const express = require('express');
+const path = require('path');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+//app.use('/api', proxy('https://xensedash-db.herokuapp.com'));
+//app.use('/api', proxy('http://localhost:3000'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
