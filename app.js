8502e765994b22d5651cde1c788df1fa8d5caf17
Thomas Vervik <thomas.vervik@gmail.com>
2018-04-25 17:23:09 +0300

    Added new proxy

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //var proxy = require('express-http-proxy');
+var proxy = require('http-proxy-middleware');
 const express = require('express');
 const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
+
 app.use(express.static(__dirname + '/dist'));
+app.use('/api', proxy({target: 'https://roots-api.herokuapp.com/api', changeOrigin:false}));
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
